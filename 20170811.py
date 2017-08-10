9cb8b3ba7aaa71dd835feed6452ca8849bd94f94
Akira Takenoshita <akirakira2012@gmail.com>
2017-08-11 02:40:26 +0900

    python_1

---
+++ 20170811.py
@@ -0,0 +1,49 @@
+Python 3.6.2 (v3.6.2:5fd33b5926, Jul 16 2017, 20:11:06) 
+[GCC 4.2.1 (Apple Inc. build 5666) (dot 3)] on darwin
+Type "copyright", "credits" or "license()" for more information.
+>>> WARNING: The version of Tcl/Tk (8.5.9) in use may be unstable.
+Visit http://www.python.org/download/mac/tcltk/ for current information.
+print("Hello World")
+Hello World
+>>> print(1+2)
+3
+>>> print([1,2,3,4])
+[1, 2, 3, 4]
+>>> a=10
+>>> print(a)
+10
+>>> print(2+1)
+3
+>>> print(2-1)
+1
+>>> print(2*3)
+6
+>>> print(6/2)
+3.0
+>>> print(5%2)
+1
+>>> a=1
+>>> a
+1
+>>> a
+1
+>>> a;a
+1
+1
+>>> a+a
+2
+>>> a=30
+>>> b=15
+>>> print(a+b)
+45
+>>> print(a+b)
+45
+>>> print(a-b)
+15
+>>> print(a*b)
+450
+>>> print(a/b)
+2.0
+>>> print(a%b)
+0
+>>> 
